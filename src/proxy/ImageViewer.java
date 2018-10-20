83f8652a44cb5e62f1f0982ee3f712d9f7099f5b
彭颖 <yingpeng@iflytek.com>
2018-10-20 10:49:27 +0800

    设计模式

---
+++ src/proxy/ImageViewer.java
@@ -0,0 +1,14 @@
+package proxy;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ImageViewer {
+    public static void main(String[] args) throws MalformedURLException {
+        String image = "http://image.jpg";
+        URL url = new URL(image);
+        HighResolutionImage highResolutionImage = new HighResolutionImage(url);
+        ImageProxy imageProxy = new ImageProxy(highResolutionImage);
+        imageProxy.showImage();
+    }
+}
